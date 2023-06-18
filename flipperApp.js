4bd27d448272cbaa74831cd4519053e8b80f49ad
indekwe <131709694+indekwe@users.noreply.github.com>
2023-06-17 21:58:09 -0700

    Update flipperApp.js

--- flipperApp.js
+++ flipperApp.js
@@ -1,10 +1,13 @@
+//an array consist of colors to be set to the background
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+//selecting html element by DOM
 const clickedButton= document.getElementById('btn');
 const backgroundColor=document.querySelector(".color")
+//function that will generate color to be set to the background from colors array randomly
 const clicking= function(){
     let randomColor=Math.floor(Math.random()*4)
     document.body.style.backgroundColor=colors[randomColor];
     backgroundColor.textContent = `${colors[randomColor]}`;
 }
-
+//connecting clicking action by user to the function which generate background colors randomly
 clickedButton.addEventListener('click', clicking)
